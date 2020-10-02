03ed7eb8542e0b5c64d6421538f925db5e8859fa
kasprzakdominik@outlook.com <44524120+Czandi@users.noreply.github.com>
2020-10-02 17:39:49 +0200

    Add spring boot blank project

---
+++ src/main/java/com/app/inventorysystemapp/InventorySystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.inventorysystemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySystemAppApplication.class, args);
+	}
+
+}
